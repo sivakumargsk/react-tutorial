61dbdc4306f725c3bb3b75260e8ad2d89dc3aaf3
sivakumargsk <geddasiva777@gmail.com>
2016-06-13 22:18:38 +0530

    completed react tutorials

--- public/scripts/tutorial12.js
+++ public/scripts/tutorial12.js
@@ -49,12 +49,33 @@ var CommentForm = React.createClass ({
 }
 );
 
+
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
   render: function(){
     return (
       <div className="CommentBox">
          <h1> Comments</h1>
-         <CommentList data={this.props.data}/>
+         <CommentList data={this.state.data} />
          <CommentForm />
       </div>
     );
@@ -62,6 +83,6 @@ var CommentBox = React.createClass({
 });
 
 ReactDOM.render (
-  <CommentBox data={data} />,
+  <CommentBox url="/api/comments" pollInterval={2000} />,
   document.getElementById('content')
 );
