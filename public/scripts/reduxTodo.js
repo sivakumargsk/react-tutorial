7dff19a276f02ceea207a12747680852882a852e
sivakumargsk <geddasiva777@gmail.com>
2016-06-27 00:00:30 +0530

    adding for each tasklist

--- public/scripts/reduxTodo.js
+++ public/scripts/reduxTodo.js
@@ -1,8 +1,12 @@
 
+var idx = 0;
+
 function todoApp (state = [], action) {
   switch (action.type) {
     case 'ADD_TODO':
-    return state.concat([action.value])
+    return state.concat([{taskName: action.taskName,
+                          taskId: idx++,
+                          completed: false} ])
     default:
     return state
   }
@@ -11,31 +15,30 @@ function todoApp (state = [], action) {
 const store = Redux.createStore (todoApp);
 
 var TodoDemo = React.createClass ({
-    render: function () {
+
+  addTodo: function () {
+     store.dispatch({ type: 'ADD_TODO',
+                      taskName: document.getElementById('taskInput').value });
+    document.getElementById('taskInput').value = '';
+  },
+  render: function () {
     return (
-      <div>
-        <input id="taskInput" type="text" />
-        <input type="button" value="Add task" onClick = {this.props.addTask} />
-        <ul> {this.props.listdata.map(function (i){
-          return (<li>{i}</li>);
-        })}
-        </ul>
-      </div>
+        <div>
+         <input id="taskInput" type="text" />
+         <input type="button" value="Add task" onClick = {this.addTodo} />
+         <ul> {this.props.listdata.map(function (i){
+           return (<li key={i.taskId} id={i.taskId}>{i.taskName}</li>);
+            })}
+         </ul>
+        </div>
     );
   }
 });
 
 function render() {
   ReactDOM.render (
-    <TodoDemo
-      listdata={store.getState()}
-      addTask={function (){
-                     store.dispatch({ type: 'ADD_TODO',
-                                      value: document.getElementById('taskInput').value })}
-                    }
-      />,
-      document.getElementById('content')
-    );
+    <TodoDemo listdata={store.getState()}/>,
+    document.getElementById('content'));
 }
 
 store.subscribe(render);
