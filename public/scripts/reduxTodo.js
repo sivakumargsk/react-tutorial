59b7e611a59bbacc02865062e4d4c7127703d1f7
sivakumargsk <geddasiva777@gmail.com>
2016-06-27 11:30:40 +0530

    Toggle todo functionality is added

--- public/scripts/reduxTodo.js
+++ public/scripts/reduxTodo.js
@@ -3,10 +3,19 @@ var idx = 0;
 
 function todoApp (state = [], action) {
   switch (action.type) {
-    case 'ADD_TODO':
+  case 'ADD_TODO':
     return state.concat([{taskName: action.taskName,
                           taskId: idx++,
                           completed: false} ])
+  case 'TOGGLE_TODO':
+    return state.map(function (i) {
+      if (i.taskId !== action.index){
+        return i;
+      }
+      return Object.assign({}, i, {
+        completed: !i.completed
+      })
+    })
     default:
     return state
   }
@@ -27,7 +36,16 @@ var TodoDemo = React.createClass ({
          <input id="taskInput" type="text" />
          <input type="button" value="Add task" onClick = {this.addTodo} />
          <ul> {this.props.listdata.map(function (i){
-           return (<li key={i.taskId} id={i.taskId}>{i.taskName}</li>);
+           return (<li
+                   key={i.taskId}
+                   style={{textDecoration: i.completed ? 'line-through' : 'none',
+                           cursor: 'pointer'}}
+                      onClick={function ()
+                                {
+                                 store.dispatch({ type: 'TOGGLE_TODO',
+                                                  index: i.taskId});
+                                }}
+                   >{i.taskName}</li>);
             })}
          </ul>
         </div>
