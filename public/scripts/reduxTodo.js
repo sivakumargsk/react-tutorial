7551877d4ddfcfe9e30bce45999fb8bcc25aa35b
sivakumargsk <geddasiva777@gmail.com>
2016-06-29 19:42:37 +0530

    Updating functionality for Active, Completed and All Todos with the help redux videos. Here we create two reducers there are visibilityFilter and todos now these two reducers are combined to single one called todoApp. now these reducer is used at redux.createStore function. and small refactoring at code

--- public/scripts/reduxTodo.js
+++ public/scripts/reduxTodo.js
@@ -1,55 +1,81 @@
 
 var idx = 0;
 
-function todoApp (state = [], action) {
+
+// todo reducer
+
+const todos = function (state = [], action) {
   switch (action.type) {
   case 'ADD_TODO':
-    return state.concat([{taskName: action.taskName,
-                          taskId: idx++,
-                          completed: false} ])
+    return state.concat(
+      [{taskName: action.taskName,
+        taskId: idx++,
+        completed: false}])
+
   case 'TOGGLE_TODO':
     return state.map(function (i) {
-      if (i.taskId !== action.index){
-        return i;
-      }
-      return Object.assign({}, i, {
-        completed: !i.completed
-      })
-    })
+      if (i.taskId == action.index) {
+         return Object.assign({}, i, {completed: !i.completed }); }
+      else {
+         return i; }
+    });
+  default:
+    return state
+  }
+}
 
-  case 'SHOW_ACTIVE':
-    return state.filter(function (i){
-      if (i.completed == false){
-        return true;
-      }else{
-        return false;
-      }
-    })
-  case 'SHOW_COMPLETED':
-    return state.filter(function (i){
-      if (i.completed == true){
-        return true;
-      }else{
-        return false;
-      }
-    })
-  case 'SHOW_ALL':
-    return state
+// visibilityFilter reducer
+
+const visibilityFilter = function (state = 'SHOW_ALL', action){
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter;
     default:
-    return state
+      return state;
+  }
+}
+
+// combining to reducers
+
+const todoApp = Redux.combineReducers({
+  todos,
+  visibilityFilter
+});
+
+// this a simple function which takes todos and show in a our needs
+
+function getVisibleTodos (todos, filter){
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos;
+
+    case 'SHOW_COMPLETED':
+      return todos.filter(function (i){
+        return i.completed; }
+      );
+    case 'SHOW_ACTIVE':
+     return todos.filter(function (i){
+       return !i.completed; }
+     );
+     default:
+      return todos;
   }
 }
 
 const store = Redux.createStore (todoApp);
 
 var TodoDemo = React.createClass ({
-
   addTodo: function () {
      store.dispatch({ type: 'ADD_TODO',
                       taskName: document.getElementById('taskInput').value });
     document.getElementById('taskInput').value = '';
   },
+
   render: function () {
+    const visibleTodos = getVisibleTodos (
+      this.props.todos,
+      this.props.visibilityFilter
+    );
     return (
         <div>
         <input id="taskInput"
@@ -59,15 +85,21 @@ var TodoDemo = React.createClass ({
                onClick = {this.addTodo} />
         <input type='button'
                value='Show All'
-               onClick = {function (){store.dispatch({ type: 'SHOW_ALL'});}} />
+               onClick = {function (){
+                 store.dispatch({ type: 'SET_VISIBILITY_FILTER',
+                                  filter: 'SHOW_ALL'});}} />
         <input type='button'
                value='Completed'
-               onClick = {function (){store.dispatch({ type: 'SHOW_COMPLETED'});}} />
+               onClick = {function (){
+                 store.dispatch({ type: 'SET_VISIBILITY_FILTER',
+                                  filter: 'SHOW_COMPLETED'});}} />
         <input type='button'
                value='Active'
-               onClick = {function (){store.dispatch({ type: 'SHOW_ACTIVE'});}} />
+               onClick = {function (){
+                 store.dispatch({ type: 'SET_VISIBILITY_FILTER',
+                                  filter: 'SHOW_ACTIVE'});}} />
         <ul>
-        {this.props.listdata.map(function (i){
+        {visibleTodos.map(function (i){
            return (<li
                    key={i.taskId}
                    style={{textDecoration: i.completed ? 'line-through' : 'none',
@@ -84,7 +116,9 @@ var TodoDemo = React.createClass ({
 
 function render() {
   ReactDOM.render (
-    <TodoDemo listdata={store.getState()}/>,
+    <TodoDemo
+    todos={store.getState().todos}
+    visibilityFilter={store.getState().visibilityFilter} />,
     document.getElementById('content'));
 }
 
